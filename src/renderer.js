bbe9eebd21a2ce31802d9fe2994c80cbcb835516
nesuwu <eryser6@gmail.com>
2024-02-29 23:17:00 +0100

    Refactored CSS and updated HTML for file upload feature

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,20 @@
 const axios = require('axios')
 const uploadButton = document.getElementById('uploadButton')
 const fileInput = document.getElementById('fileInput')
+const fileButton = document.getElementById('fileButton')
 const userhashInput = document.getElementById('userhashInput')
+
+fileButton.addEventListener('click', () => {
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', () => {
+  uploadButton.disabled = !fileInput.value;
+});
 
 uploadButton.addEventListener('click', () => {
   const file = fileInput.files[0]
-  const userhash = userhashInput.value
+  let userhash = userhashInput.value
   if (file) {
     const form = new FormData()
     form.append('reqtype', 'fileupload')
@@ -17,6 +26,44 @@ uploadButton.addEventListener('click', () => {
     axios.post('https://catbox.moe/user/api.php', form)
     .then(response => {
       console.log('File uploaded successfully. URL:', response.data)
+
+      // Create a new button
+      const button = document.createElement('button');
+      button.id = 'linkButton';
+      button.style.display = 'none'; // Initially hidden
+
+      // Set the button's text to the URL of the uploaded file
+      button.textContent = response.data;
+
+      // Add a click event listener to the button
+      button.addEventListener('click', () => {
+        // Copy the link to the clipboard
+        navigator.clipboard.writeText(response.data).then(() => {
+          console.log('Link copied to clipboard');
+        }, (err) => {
+          console.error('Error copying link to clipboard:', err);
+        });
+      });
+
+      // Append the button after the upload button
+      uploadButton.parentNode.insertBefore(button, uploadButton.nextSibling);
+
+      // Trigger the transition
+      setTimeout(function() {
+        button.style.display = 'block'; // This will trigger the transition
+
+        // Trigger the animation
+        button.animate([
+          // keyframes
+          { transform: 'scale(0)' }, 
+          { transform: 'scale(1)' }
+        ], { 
+          // timing options
+          duration: 1000,
+          iterations: 1,
+          easing: 'ease',
+        });
+      }, 0);
     })
     .catch(error => {
       console.log('Error uploading file:', error)
